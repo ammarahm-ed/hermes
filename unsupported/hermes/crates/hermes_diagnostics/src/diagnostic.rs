682fbe92584ab03f09552f53b0aeb5e51d9828da
generatedunixname1364019048813598 <noreply+1364019048813598@fb.com>
2026-06-22 10:27:55 -0700

    Fix unused_parens issues in xplat/static_h/unsupported/hermes/crates/hermes_diagnostics/src
    Reviewed By: diliop
    
    Differential Revision: D109274025
    
    fbshipit-source-id: 0f2a17cde3e46dc0126d2944932edd3ee3558cdf

--- unsupported/hermes/crates/hermes_diagnostics/src/diagnostic.rs
+++ unsupported/hermes/crates/hermes_diagnostics/src/diagnostic.rs
@@ -262,6 +262,6 @@ impl From<Diagnostic> for Diagnostics {
 fn source_span_from_range(range: SourceRange) -> SourceSpan {
     SourceSpan::new(
         (range.start as usize).into(),
-        ((range.end - range.start) as usize),
+        (range.end - range.start) as usize,
     )
 }
